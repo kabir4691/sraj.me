6ef0baf20f809aa485cf121345749b46bc400fda
Shantanu Raj <s@sraj.me>
2019-06-20 14:04:17 +0530

    Switch to TypeScript

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -6,11 +6,17 @@
  */
 
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { useStaticQuery, graphql } from 'gatsby'
 
-function SEO({ description, lang, meta, title }) {
+interface ISEOProps {
+  description?: string
+  lang: string
+  meta: JSX.IntrinsicElements['meta'][]
+  title: string
+}
+
+function SEO({ description, lang, meta, title }: ISEOProps) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -67,7 +73,7 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ].concat(meta)}
+      ].concat(meta as any)}
     />
   )
 }
@@ -76,13 +82,6 @@ SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
