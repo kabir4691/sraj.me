8f0b947ea22d72c5a8d38e47a520ed948e8f9d49
Shantanu Raj <s@sraj.me>
2019-06-21 00:59:54 +0530

    Guard theme preference check with browser check

--- src/utils/theme-preference.ts
+++ src/utils/theme-preference.ts
@@ -1,8 +1,9 @@
 import { ITheme } from '../type';
 import { Storage } from './storage';
+import { isBrowser } from './is-browser';
 
 export const preferredTheme = (): ITheme['theme'] => {
-  const isLightModeSystem = matchMedia('(prefers-color-scheme: light)').matches;
+  const isLightModeSystem = isBrowser() && matchMedia('(prefers-color-scheme: light)').matches;
   const hasPreviousPreference = Storage.getTheme();
 
   if (hasPreviousPreference) {
