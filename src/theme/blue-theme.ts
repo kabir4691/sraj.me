9b17746ca89a3cf432ea7fab424cb16506f6fb1e
Shantanu Raj <s@sraj.me>
2019-06-27 00:54:44 +0530

    Accord for @SuperGeekJake's request

--- src/theme/blue-theme.ts
+++ src/theme/blue-theme.ts
@@ -2,7 +2,7 @@ import { ITheme } from '../type';
 
 export const blueTheme: ITheme = {
   theme: 'blue',
-  background: '#0032ee',
+  background: '#495492',
   text: '#fff1c2',
   accent: '#fff1c2',
 };
